7f125ab694c338b0b77647de8e631aed75feb0b6
Milen Petrinski <gonzo@greatgonzo.net>
2017-10-27 18:13:51 +0300

    Check if container exists before attaching handlers.

--- paspartou.js
+++ paspartou.js
@@ -10,77 +10,88 @@ if( typeof domready != 'function' ){
 }
 
 domready(function() {
-	var images = new Array();
-	var current = 0;
 	var container = document.getElementById('gallery');
-	var $links = container.getElementsByTagName('a');
-	var $lb = document.getElementById('paspartou');
-	var $im = null;
-	var $p = null;
-	var $next = document.getElementById('next');
-	var $prev = document.getElementById('prev');
-	var addEventListener = (container.addEventListener)?'addEventListener':'attachEvent';
-	var click = (addEventListener == 'addEventListener')?'click':'onclick';
+	if (container) {
+		var images = new Array();
+		var current = 0;
+		var $links = container.getElementsByTagName('a');
+		var $lb = document.getElementById('paspartou');
+		var $im = null;
+		var $p = null;
+		var $next = document.getElementById('next');
+		var $prev = document.getElementById('prev');
+		var addEventListener = (container.addEventListener)?'addEventListener':'attachEvent';
+		var click = (addEventListener == 'addEventListener')?'click':'onclick';
+		var focus = (addEventListener == 'addEventListener')?'focus':'onfocus';
+		var blur = (addEventListener == 'addEventListener')?'blur':'onblur';
 
-	function setButtons(current){
-		if(current == images.length - 1){
-			$next.className = 'hidden';
+		function setButtons(current){
+			if(current == images.length - 1){
+				$next.className = 'hidden';
+			}
+			else{
+				$next.className = '';
+			}
+			if(current == 0){
+				$prev.className = 'hidden';
+			}
+			else{
+				$prev.className = '';
+			}
 		}
-		else{
-			$next.className = '';
-		}
-		if(current == 0){
-			$prev.className = 'hidden';
-		}
-		else{
-			$prev.className = '';
-		}
+
+		for(var i = 0, ll = $links.length; i < ll; i++){
+			images.push( {
+				url: $links[i].href,
+				title: ($links[i].getAttribute('data-title') != null)?$links[i].getAttribute('data-title'):''
+			} );
+			(function(link, index){
+				link[addEventListener](click, function(e){
+					current = index;
+					var title = (link.getAttribute('data-title') != null)?link.getAttribute('data-title'):'';
+					$lb.getElementsByTagName('div')[0].innerHTML = '<div><img src="'+link.href+'" class="hidden" alt="' + title + '" ><p>' + title + '</p></div>';
+					$im = $lb.getElementsByTagName('img')[0];
+					$p = $lb.getElementsByTagName('p')[0];
+					$im.onload = function(){
+						this.className = '';
+						$p.innerHTML = this.getAttribute('alt');
+					};
+					$lb.className = 'active';
+					setButtons(index);
+					if (addEventListener == 'addEventListener') {
+						e.preventDefault();
+					}
+					return false;
+				}, false);
+				link[addEventListener](focus, function(e){
+					this.parentNode.className = 'focus';
+				}, false);
+				link[addEventListener](blur, function(e){
+					this.parentNode.className = '';
+				}, false)
+			})($links[i], i);
+		};
+		$next[addEventListener](click, function(e){
+			if(current < images.length - 1){
+				current++;
+				$im.className = 'hidden';
+				$im.src = images[current].url;
+				$im.setAttribute('alt', images[current].title);
+				setButtons(current);
+			}
+		}, false);
+		$prev[addEventListener](click, function(e){
+			if(current > 0){
+				current--;
+				$im.className = 'hidden';
+				$im.src = images[current].url;
+				$im.setAttribute('alt', images[current].title);
+				setButtons(current);
+			}
+		}, false);
+		document.getElementById('close')[addEventListener](click, function(e){
+			$lb.className = '';
+		}, false);
 	}
 
-	for(var i = 0, ll = $links.length; i < ll; i++){
-		images.push( {
-			url: $links[i].href,
-			title: ($links[i].getAttribute('data-title') != null)?$links[i].getAttribute('data-title'):''
-		} );
-		(function(link, index){
-			link[addEventListener](click, function(e){
-				current = index;
-				var title = (link.getAttribute('data-title') != null)?link.getAttribute('data-title'):'';
-				$lb.getElementsByTagName('div')[0].innerHTML = '<div><img src="'+link.href+'" class="hidden" alt="' + title + '" ><p>' + title + '</p></div>';
-				$im = $lb.getElementsByTagName('img')[0];
-				$p = $lb.getElementsByTagName('p')[0];
-				$im.onload = function(){
-					this.className = '';
-					$p.innerHTML = this.getAttribute('alt');
-				};
-				$lb.className = 'active';
-				setButtons(index);
-				if (addEventListener == 'addEventListener') {
-					e.preventDefault();
-				}
-				return false;
-			}, false);
-		})($links[i], i);
-	};
-	$next[addEventListener](click, function(e){
-		if(current < images.length - 1){
-			current++;
-			$im.className = 'hidden';
-			$im.src = images[current].url;
-			$im.setAttribute('alt', images[current].title);
-			setButtons(current);
-		}
-	}, false);
-	$prev[addEventListener](click, function(e){
-		if(current > 0){
-			current--;
-			$im.className = 'hidden';
-			$im.src = images[current].url;
-			$im.setAttribute('alt', images[current].title);
-			setButtons(current);
-		}
-	}, false);
-	document.getElementById('close')[addEventListener](click, function(e){
-		$lb.className = '';
-	}, false);
 });
